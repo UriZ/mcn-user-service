de973aa2188529b1b93f0df1b0c528c4dee1f76c
URIZ <uriz@il.ibm.com>
2018-02-04 13:38:18 +0200

    basic impl of create user

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,46 @@
 'use strict';
+require ('request');
 
 
-module.exports.createUser = function createUser (req, res, next) {
+
+let createUserData = (userId, userName, email)=>{
+    let userRecord = {
+        "userID": userId,
+        "userName": userName,
+        "email": email,
+        "preferences":{
+            "key":"val"
+        }
+    };
+
+
+    return userRecord;
+
+};
+
+
+module.exports.createUser = function createUser (req, res) {
+
+
+    console.log("in create user");
+    if (!req.swagger.params.fb_user_id){
+        console.log("missing id");
+    }
+    // get id from request
+    const userID = req.swagger.params.fb_user_id.value;
+    const userName = req.swagger.params.fb_user_name.value;
+    const email = req.swagger.params.email.value;
+
+    console.log(userID);
+
+    let record = createUserData(userID,userName, email);
+
+    console.log(JSON.stringify(record));
+
+
+    // const certificateId = request.swagger.params.certificate_id.value;
+
+
+
     res.send("yes!!!!");
 };
