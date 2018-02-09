d781cf8d892c66dbf0823f842cebe5609240fdf1
URIZ <uriz@il.ibm.com>
2018-02-09 15:54:25 +0200

    error handling in create user (if already exists)

--- controllers/users.js
+++ controllers/users.js
@@ -29,9 +29,10 @@ const insertUserToDB = function(db, callback, userDoc) {
     // Insert some documents
     collection.insert(
         userDoc, (err, result)=> {
-        assert.equal(err, null);
-        console.log("Inserted user documents into collection");
-        callback(result);
+
+        // assert.equal(err, null);
+        // console.log("Inserted user documents into collection");
+        callback(err,result);
     });
 }
 
@@ -82,7 +83,6 @@ module.exports.getUser = function getUser (req, res){
  */
 module.exports.createUser = function createUser (req, res) {
 
-
     console.log("in create user");
     if (!req.swagger.params.fb_user_id){
         console.log("missing id");
@@ -92,28 +92,35 @@ module.exports.createUser = function createUser (req, res) {
     const userName = req.swagger.params.fb_user_name.value;
     const email = req.swagger.params.email.value;
 
-    console.log(fbUserID);
-
     let record = createUserData(fbUserID,userName, email);
 
+    // db url
     const url = process.env.DB_URL;
 
     // Database Name
     const dbName = process.env.DB_NAME;
 
 
-    // Use connect to the server
+    // connect to the server
     MongoClient.connect(url, function(err, client) {
         assert.equal(null, err);
         console.log("Connected to db");
 
         const db = client.db(dbName);
 
-        insertUserToDB(db, function(result) {
-            console.log("document inserted");
-            console.log(result.ops);
-            client.close();
-            res.send(200);
+        insertUserToDB(db, function(err,result) {
+
+
+            if (err){
+                res.status(500).send(err);
+            }
+            else{
+                console.log("document inserted");
+                console.log(result.ops);
+                client.close();
+                res.send(200);
+            }
+
         }, record);
     });
 
